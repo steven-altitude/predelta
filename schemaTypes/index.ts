33fe48249b44cef8e0fc561a79fb21a9d66ab700
Steven Luzuriaga <75744001+steven-altitude@users.noreply.github.com>
2025-07-24 15:56:33 -0500

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,16 @@
-export const schemaTypes = []
+
+import nivel from './nivel'
+import proyecto from './proyecto'
+import testimonial from './testimonial'
+import galleryImage from './galleryImage'
+import pilarsSection from './pilarsSection'
+import page from './page'
+
+export const schemaTypes = [
+  nivel,
+  proyecto,
+  testimonial,
+  galleryImage,
+  pilarsSection,
+  page
+]

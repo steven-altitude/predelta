0a37720a78d11ea3b189daf5a112b929de55a327
Steven Luzuriaga <75744001+steven-altitude@users.noreply.github.com>
2025-07-24 17:26:39 -0500

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,7 +1,6 @@
 import { pageSchema } from "./page"
 import { pilarsSectionSchema } from "./pilarsSection"
 import { featuresSectionSchema } from "./featuresSection"
-import { testimonialsSectionSchema } from "./testimonialsSection"
 import { nivelesSectionSchema } from "./nivelesSection"
 import { gallerySectionSchema } from "./gallerySection"
 import { ctaSectionSchema } from "./ctaSection"
@@ -21,7 +20,6 @@ export const schemaTypes = [
   // Secciones para páginas
   pilarsSectionSchema,
   featuresSectionSchema,
-  testimonialsSectionSchema,
   nivelesSectionSchema,
   gallerySectionSchema,
   ctaSectionSchema,

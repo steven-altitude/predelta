54610874ba0fa295b0e68f74e1218c65b98fe8d3
Steven Luzuriaga <75744001+steven-altitude@users.noreply.github.com>
2025-07-24 16:29:29 -0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,30 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from "sanity"
+import { deskTool } from "sanity/desk"
+import { visionTool } from "@sanity/vision"
+import { schemaTypes } from "./sanity/schemas"
 
 export default defineConfig({
-  name: 'default',
-  title: 'Uniddad Educativa Presco',
+  name: "predelta-website",
+  title: "Predelta Website",
 
-  projectId: 'wc3qeypc',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    deskTool({
+      structure: (S) =>
+        S.list()
+          .title("Contenido")
+          .items([
+            S.listItem().title("Páginas").child(S.documentTypeList("page").title("Páginas")),
+            S.listItem().title("Niveles Educativos").child(S.documentTypeList("nivel").title("Niveles")),
+            S.listItem().title("Proyectos Especiales").child(S.documentTypeList("proyecto").title("Proyectos")),
+            S.listItem().title("Testimonios").child(S.documentTypeList("testimonial").title("Testimonios")),
+            S.listItem().title("Galería").child(S.documentTypeList("galleryImage").title("Imágenes")),
+          ]),
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
